cd4c33d462f2702457a0e1c859cbb27e0b24c5bc
WhoReadThisWillDie <msadkoff@mail.ru>
2023-12-07 13:16:23 +0100

    Main menu design changes
    - New button design and font added
    - Added simple animation for background in main menu

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,14 @@
 using System;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
     private Rigidbody2D rb;
     private Vector2 input;
     private Animator animator;
+
+    [SerializeField] private float speed;
 
     private void Start()
     {
@@ -21,23 +23,28 @@ public class PlayerMovement : MonoBehaviour
 
         input.Normalize();
 
-        if (input.x != 0) 
+        ManageAnimations();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = input * speed;
+    }
+
+    private void ManageAnimations()
+    {
+        if (input.x != 0)
         {
             animator.SetFloat("X", input.x);
             animator.SetBool("IsWalking", true);
         }
-        else if (input.y != 0) 
+        else if (input.y != 0)
         {
             animator.SetBool("IsWalking", true);
         }
         else if (input.x == 0 && input.y == 0)
         {
             animator.SetBool("IsWalking", false);
-        } 
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = input * speed;
+        }
     }
 }
