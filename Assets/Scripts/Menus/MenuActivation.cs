af512a8aa439c821c2cd95e9a6726648f677243d
DanilDuckk <danylo.kurbatov@gmail.com>
2024-01-25 12:43:41 +0100

    Tutorial added

--- Assets/Scripts/Menus/MenuActivation.cs
+++ Assets/Scripts/Menus/MenuActivation.cs
@@ -1,10 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using Update = UnityEngine.PlayerLoop.Update;
 
 public class MenuActivation : MonoBehaviour
 {
+    public GameObject tutorialPage1;
+    public GameObject[] buttons;
+    public GameObject[] menuPages;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("MainLobby");
@@ -14,4 +22,40 @@ public class MenuActivation : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void ShowTutorial()
+    {
+        tutorialPage1.SetActive(true);
+        buttons[0].SetActive(true);
+    }
+
+    public void SkipSlide1()
+    {
+       menuPages[1].SetActive(true);
+       buttons[1].SetActive(true);
+    }
+    public void SkipSlide2()
+    {
+        menuPages[2].SetActive(true);
+        buttons[2].SetActive(true);
+
+    }
+    public void SkipSlide3()
+    {
+        menuPages[3].SetActive(true);
+        buttons[3].SetActive(true);
+
+    }
+    public void SkipSlide4()
+    {
+        foreach (var page in menuPages)
+        {
+            page.SetActive(false);
+            buttons[0].SetActive(false);
+            buttons[1].SetActive(false);
+            buttons[2].SetActive(false);
+            buttons[3].SetActive(false);
+        }    
+    }
+    
 }
